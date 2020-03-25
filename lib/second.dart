a6f27187c7ee9987134bea417b89f8b6264815ba
AbijithKaaliyan <abhijithachu0@gmail.com>
2020-03-25 19:43:52 +0530

    updated files

--- lib/second.dart
+++ lib/second.dart
@@ -22,14 +22,167 @@ class Second extends StatelessWidget {
           ),
 
                 Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    CircleAvatar(
-                      backgroundImage: AssetImage('assets/naruto.jpg'),
-                      radius: 50,
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(30, 30, 0, 0),
+                      child: Row(
+                        children: <Widget>[
+                          CircleAvatar(
+                            backgroundImage: AssetImage('assets/naruto.jpg'),
+                            radius: 50,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(30, 0, 0, 0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: <Widget>[
+                                Text('NARUTO UZUMAKI',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    letterSpacing: 1,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text('Hookage of the Hidden Leaf\n' 'Son of 4th Hookage\n'
+                                'Strongest shinobi of this Era',
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  letterSpacing: 1,
+                                  color: Colors.grey,
+                                ),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
 
+                  ],
+                ),
+          SizedBox(
+            height: 50,
+          ),
+          Container(
+            height:70,
+            width: 150,
+            child: Row(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(60, 0, 0, 0),
+                  child: Column(
+                    children: <Widget>[
+                      Text('photos',
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text ('2000',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(60, 0, 0, 0),
+                  child: Column(
+                    children: <Widget>[
+                      Text('Followers',
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text('15000',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(60, 0, 0, 0),
+                  child: Column(
+                    children: <Widget>[
+                      Text('Follows',
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text('350',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Center(
+            child: Container(
+              width: 350,
+              child: Card(
+                elevation: 30,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: const ListTile(
+                        leading: Icon(Icons.reply),
+                        title: const Text.rich(
+                          TextSpan(
+                            children: <TextSpan>[
+                              TextSpan(text: 'Replied by   ',
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              TextSpan(
+                                text: 'Konahamaru Sarutobi',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ]
+                          ),
+                        ),
+                        subtitle: Text('\nThat was an amazing jutsu man!!.'
+                            'Really excited and looking forward to learn Senjutsu',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ),
                     ),
                   ],
-                )
+                ),
+              ),
+            ),
+          ),
         ],
       ),
 
